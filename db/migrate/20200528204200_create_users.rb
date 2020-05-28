91c532131521e8c015452aad2e2e0ee9af5a9dec
robond <shubham88singh@gmail.com>
2020-05-28 15:03:18 -0700

    Adding the db schemads

--- db/migrate/20200528204200_create_users.rb
+++ db/migrate/20200528204200_create_users.rb
@@ -2,6 +2,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
+      t.string :email
+      t.string :password
+      
+      t.datetime 'created_at'
+      t.datetime 'updated_at'
+      
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_05_28_204200) do
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
