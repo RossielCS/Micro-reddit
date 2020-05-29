8ba4385496b7ed3fbeaa54bd59f4e3eb0a635683
Rossiel Carranza <rossielcarranza@gmail.com>
2020-05-29 12:46:36 -0300

    Add schema for posts

--- db/migrate/20200529151148_create_posts.rb
+++ db/migrate/20200529151148_create_posts.rb
@@ -1,9 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.references :user
+
       t.string :title
       t.text :body
-      t.integer :author_id
+      t.integer :user_id
 
       t.datetime 'created_at'
       t.datetime 'updated_at'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_204200) do
+ActiveRecord::Schema.define(version: 2020_05_29_151148) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
