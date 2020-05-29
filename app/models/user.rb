ff5d5a13b22927f8af864ace57afb2b1385b5b31
shubshk88 <shubhs@LAPTOP-59G32S73.localdomain>
2020-05-29 22:30:21 +0530

    miigration error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 4 }
+  has_many :posts
 end
---
+++ db/migrate/20200529164241_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.references :user, foreign_key: true
+      t.string :title
+      t.text :body
+      t.integer :user_id
+
+      t.datetime 'created_at'
+      t.datetime 'updated_at'
+
+
+      t.timestamps
+    end
+  end
+end
