756802f446949aa7b444f47a795561fe6ddeb346
Rossiel Carranza <rossielcarranza@gmail.com>
2020-05-29 12:58:24 -0300

    Change author_id for user_id in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :title, :body, :author_id, presence: true
+  validates :title, :body, :user_id, presence: true
   validates :title, uniqueness: true
 end
