f6a3047bd96ac622f9cfe5466be8f3602dd98d3c
shubshk88 <shubhs@LAPTOP-59G32S73.localdomain>
2020-05-29 23:40:44 +0530

    Finishing the comment section

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 4 }
   has_many :posts
+  has_many :comments
 end
--- db/migrate/20200529164241_create_posts.rb
+++ db/migrate/20200529164241_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: false
       t.string :title
       t.text :body
       t.integer :user_id
